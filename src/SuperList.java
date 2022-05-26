c857dac7c1c3983e8eeeb8a5a9f45140b2b09136
FernandoPerera <fernando02perera@gmail.com>
2022-05-26 09:47:37 +0100

    Feature: Creados los métodos extendidos de la clase abstracta en la clase SuperList y clase instanciada en el main

--- src/SuperList.java
+++ src/SuperList.java
@@ -1,2 +1,46 @@
-public class SuperList {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SuperList extends ListManager{
+
+    ArrayList<Object> collection = new ArrayList<Object>();
+
+    @Override
+    public void addElement(Object element) {
+        collection.add(collection.size(), element);
+    }
+
+    @Override
+    public Object removeElement(int position) {
+
+        Object valor = collection.get(position);
+
+        collection.remove(position);
+
+        return valor;
+    }
+
+    @Override
+    public void showElements() {
+
+        Iterator<Object> it = collection.iterator();
+
+        while (it.hasNext()) {
+            Object valor = it.next();
+            System.out.println(valor);
+        }
+    }
+
+
+    @Override
+    public void showReversedElements() {
+
+        Iterator<Object> it = collection.iterator();
+
+        while (it.hasNext()) {
+            Object valor = it.next();
+            System.out.println(valor);
+        }
+
+    }
 }
